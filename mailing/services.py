1e74a459bc497471185bee4e7043f7e46d1f5f4a
gtsoktoeva001 <galina-tsoktoeva@mail.ru>
2023-08-02 22:40:21 +0300

    кеширование

--- mailing/services.py
+++ mailing/services.py
@@ -9,7 +9,7 @@ def get_cache_version_for_article(article_pk):
         key = f'article_list{article_pk}'
         article_list = cache.get(key)
         if article_list is None:
-            version_list = Article.objects.filter(article__pk=article_pk)
+            article_list = Article.objects.filter(article__pk=article_pk)
             cache.set(key, article_list)
     else:
         article_list = Article.objects.filter(article__pk=article_pk)
