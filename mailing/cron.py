7bbaab8bd0ca2a3df02be70b9e1df23776408b44
gtsoktoeva001 <galina-tsoktoeva@mail.ru>
2023-08-05 19:34:11 +0300

    Отправка рассылки по расписанию

--- mailing/cron.py
+++ mailing/cron.py
@@ -10,21 +10,23 @@ from mailing.models import Mailing, Log
 
 
 def my_scheduled_job():
-    mailings = Mailing.objects.all()
+    mailings = Mailing.objects.filter(status=2)
+
     tz = pytz.timezone('Europe/Moscow')
-    clients = [client.email for client in Client.objects.all()]
+
     for new_mailing in mailings:
-        if new_mailing.status == 2:
-            if new_mailing.mailing_time <= datetime.now(tz):
-                mail_subject = new_mailing.massage.body if new_mailing.massage is not None else 'Рассылка'
-                message = new_mailing.massage.theme if new_mailing.massage is not None else 'Вам назначена рассылка'
-                try:
-                    send_mail(mail_subject, message, settings.EMAIL_HOST_USER, clients)
-                    log = Log.objects.create(date_attempt=datetime.now(tz), status='Успешно', answer='200')
-                    log.save()
-                except smtplib.SMTPException as err:
-                    log = Log.objects.create(date_attempt=datetime.now(tz), status='Ошибка', answer=err)
-                    log.save()
-                    #raise err
-                new_mailing.status = 3
-                new_mailing.save()
+        clients = [client.email for client in Client.objects.filter(user=new_mailing.user)]
+        if new_mailing.mailing_time >= datetime.now(tz):
+            mail_subject = new_mailing.massage.body if new_mailing.massage is not None else 'Рассылка'
+            message = new_mailing.massage.theme if new_mailing.massage is not None else 'Вам назначена рассылка'
+            try:
+                send_mail(mail_subject, message, settings.EMAIL_HOST_USER, clients)
+                log = Log.objects.create(date_attempt=datetime.now(tz), status='Успешно', answer='200')
+                log.save()
+            except smtplib.SMTPException as err:
+                log = Log.objects.create(date_attempt=datetime.now(tz), status='Ошибка', answer=err)
+                log.save()
+                # raise err
+            new_mailing.status = 3
+            new_mailing.save()
+
